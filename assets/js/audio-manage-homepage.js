0c48d81cfc96b648d31a408c5c37e74745a6ee9d
Vlad Vancea <vavl1996>
2018-01-16 21:28:27 +0200

    add sounds for homepage on mouse over buttons

---
+++ assets/js/audio-manage-homepage.js
@@ -0,0 +1,67 @@
+//
+// var beepOne = $("#beep-one")[0];
+// $("#rimshot-hover-sound")
+//     .mouseenter(function() {
+//         beepOne.play();
+//     });
+//
+//
+//
+//
+//
+// var rimShotAudio = document.getElementById("beep-one");
+//
+// function playRim() {
+//     rimShotAudio.play();
+// }
+// item.addEventListener('mouseover', rimShotAudio.playRim, false);
+
+function playClipLearn() {
+    if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+        if (document.all)
+        {
+            document.all.sound.src = "../audio/Invatare.mp3";
+        }
+    }
+
+    else {
+        {
+            var audio = document.getElementById("sound-for-learn");
+            // audio.play();
+            audio.play();
+        }
+    }
+}
+
+function playClipDeep() {
+    if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+        if (document.all)
+        {
+            document.all.sound.src = "../audio/Aprofunda.mp3";
+        }
+    }
+
+    else {
+        {
+            var audio = document.getElementById("sound-for-deep");
+            audio.play();
+        }
+    }
+}
+
+function playClipPlay() {
+    if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+        if (document.all)
+        {
+            document.all.sound.src = "../audio/Joc.mp3";
+        }
+    }
+
+    else {
+        {
+            var audio = document.getElementById("sound-for-play");
+            audio.play();
+        }
+    }
+}
+
